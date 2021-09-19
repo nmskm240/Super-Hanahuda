000721aafe7feeb9beaf8afb8f3daacbb9584cf8
nmskm240 <bokaronogod@gmail.com>
2021-09-19 13:35:41 +0900

    deal cards

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using SuperHanahuda.Game;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Deck _deck;
+    [SerializeField]
+    private Transform _enemyHand;
+    [SerializeField]
+    private Transform _field;
+    [SerializeField]
+    private Transform _playerHand;
+
+    private void Awake()
+    {
+        _deck.Shuffle();
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                var card = _deck.Draw();
+                card.transform.SetParent(_playerHand);
+            }
+            for (int j = 0; j < 2; j++)
+            {
+                var card = _deck.Draw();
+                card.transform.SetParent(_field);
+            }
+            for (int j = 0; j < 2; j++)
+            {
+                var card = _deck.Draw();
+                card.transform.SetParent(_enemyHand);
+            }
+        }
+    }
+}
