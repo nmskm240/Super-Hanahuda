a9a93ce5b11280e1da809a513efbcdbb926a28c3
nmskm240 <bokaronogod@gmail.com>
2021-09-23 11:31:51 +0900

    multi scene initalize

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,45 +1,10 @@
 using UnityEngine;
-using SuperHanahuda.Game;
-using SuperHanahuda.UI;
+using MultiSceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private Deck _deck;
-    [SerializeField]
-    private Transform _enemyHand;
-    [SerializeField]
-    private Transform _field;
-    [SerializeField]
-    private Transform _playerHand;
-
     private void Awake()
     {
-        _deck.Shuffle();
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                var card = _deck.Draw();
-                card.transform.SetParent(_playerHand);
-                card.transform.localScale = Vector3.one * 1.2f;
-                card.GetComponent<CardController>().View.ToggleFace();
-            }
-            for (int j = 0; j < 2; j++)
-            {
-                var card = _deck.Draw();
-                card.transform.SetParent(_field);
-                card.transform.localScale = Vector3.one;
-                card.GetComponent<DragObject>().enabled = false;
-                card.GetComponent<CardController>().View.ToggleFace();
-            }
-            for (int j = 0; j < 2; j++)
-            {
-                var card = _deck.Draw();
-                card.transform.SetParent(_enemyHand);
-                card.transform.localScale = Vector3.one * 1.2f;
-                card.GetComponent<DragObject>().enabled = false;
-            }
-        }
+        MultiSceneManager.Init();
     }
 }
