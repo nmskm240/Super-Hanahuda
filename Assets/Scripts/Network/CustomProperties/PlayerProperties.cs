a941817b666a1b82375b52132d3a3c18be0fc471
nmskm240 <bokaronogod@gmail.com>
2021-09-30 13:47:50 +0900

    create player custom properties

---
+++ Assets/Scripts/Network/CustomProperties/PlayerProperties.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Hashtable = ExitGames.Client.Photon.Hahstable;
+using Photon.Pun;
+using Photon.Realtime;
+using SuperHanahuda.Game;
+
+namespace SuperHanahuda.Network.CustomProperties
+{
+    public static class PlayerProperties
+    {
+        public static readonly string HandPropKey = "hand";
+
+        private static readonly Hashtable _hashtable = new Hashtable();
+
+        public static void SetHand(this Player player, IEnumerable<CardModel> cards)
+        {
+            _hashtable[HandPropKey] = cards.ToArray();
+        }
+
+        public static List<CardModel> GetHand(this Player player)
+        {
+            return new List<CardModel>(player.CustomProperties[HandPropKey] as IEnumerable<CardModel>);
+        }
+
+        public static void SetPlayerProperties(this Player player)
+        {
+            if (_hashtable.Count > 0)
+            {
+                player.SetCustomProperties(_hashtable);
+                _hashtable.Clear();
+            }
+        }
+    } 
+}
