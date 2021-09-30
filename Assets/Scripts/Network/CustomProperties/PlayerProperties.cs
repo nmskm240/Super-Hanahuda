b69eef7466c3d0245602c6e7b103b939a7914547
nmskm240 <bokaronogod@gmail.com>
2021-09-30 14:59:38 +0900

    fix player custom properties

--- Assets/Scripts/Network/CustomProperties/PlayerProperties.cs
+++ Assets/Scripts/Network/CustomProperties/PlayerProperties.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using Hashtable = ExitGames.Client.Photon.Hahstable;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 using Photon.Pun;
 using Photon.Realtime;
 using SuperHanahuda.Game;
