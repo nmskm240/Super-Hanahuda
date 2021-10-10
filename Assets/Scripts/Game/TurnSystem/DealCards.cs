906b2307c1fd75eac099b1116ce81a56ff7e48b4
nmskm240 <bokaronogod@gmail.com>
2021-10-10 22:06:37 +0900

    card deal

--- Assets/Scripts/Game/TurnSystem/DealCards.cs
+++ Assets/Scripts/Game/TurnSystem/DealCards.cs
@@ -1,15 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
 namespace SuperHanahuda.Game.TurnSystem
 {
     [System.Serializable]
     public class DealCards : IStep<IEnumerable<GameObject>>
     {
+        [SerializeField]
+        private Transform _target;
+
         public void Execute(IEnumerable<GameObject> objects)
         {
-            Debug.Log(objects);
+            foreach(var obj in objects)
+            {
+                obj.transform.SetParent(_target);
+            }
         }
     }
 }
